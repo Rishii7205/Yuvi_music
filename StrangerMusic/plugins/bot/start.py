bc4e6cb8cd29456482f48e349da843119612f621
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2023-09-12 08:32:25 +0530

    Update start.py

--- StrangerMusic/plugins/bot/start.py
+++ StrangerMusic/plugins/bot/start.py
@@ -41,7 +41,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CdbIJ1QACBuhkvQUuBcqrTPTeLYARKBUDFn0RqAACPQwAAvv16FVgo7OXyFd8tC8E")
+            await message.reply_sticker("CAACAgUAAxkBAAJAImT_00f6ltYX9NXeuNVP4xQhcx2BAAK0CQAC3avJVCCAb7yeIdxlMAQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
@@ -190,7 +190,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAx0CdbIJ1QACBuhkvQUuBcqrTPTeLYARKBUDFn0RqAACPQwAAvv16FVgo7OXyFd8tC8E")
+                await message.reply_sticker("CAACAgUAAxkBAAJAImT_00f6ltYX9NXeuNVP4xQhcx2BAAK0CQAC3avJVCCAb7yeIdxlMAQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
